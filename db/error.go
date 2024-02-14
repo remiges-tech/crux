c09b2197ef4987a7f9c4b342d0a87430b5e38f5c
tusharb <tushar.bhingare@merce.co>
2024-02-14 15:40:17 +0530

    add func HandleDatabaseError #39 #40

--- db/error.go
+++ db/error.go
@@ -15,9 +15,9 @@ func ErrorMessage(err error) wscutils.ErrorMessage {
 
 	if errors.As(err, &pgErr) {
 		switch pgErr.Code {
-		case "23505":
+		case "23505": //unique_violation
 			return wscutils.BuildErrorMessage(server.MsgId_Invalid_Request, server.ErrCode_AlreadyExist, &pgErr.ConstraintName)
-		case "23503":
+		case "23503": //foreign_key_violation
 			return wscutils.BuildErrorMessage(server.MsgId_Invalid_Request, server.ErrCode_NotFound, &pgErr.ConstraintName)
 		}
 	} else if err.Error() == "no rows in result set" {
@@ -25,7 +25,8 @@ func ErrorMessage(err error) wscutils.ErrorMessage {
 		return wscutils.BuildErrorMessage(1006, server.ErrCode_InvalidRequest, &feild)
 	} else {
 		return wscutils.BuildErrorMessage(server.MsgId_InternalErr, server.ErrCode_DatabaseError, nil)
-
 	}
 	return wscutils.ErrorMessage{}
 }
+
+//:"ERROR: cached plan must not change result type (SQLSTATE 0A000)"}
